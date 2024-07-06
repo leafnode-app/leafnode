a186e6b6baaee1712d239137f1404013172021e1
Torean Joel <toreanjoel@gmail.com>
2024-07-06 23:53:21 +0200

    feat: add the ability to change the token

--- lib/leaf_node_web/live/node/nodes.ex
+++ lib/leaf_node_web/live/node/nodes.ex
@@ -5,8 +5,12 @@ defmodule LeafNodeWeb.NodesLive do
     Init func that is run on init of the view
   """
   def mount(_params, _session, socket) do
-    # get the new list that we send off
-    {:ok, assign(socket, :nodes, get_nodes(socket))}
+
+    %{ id: id, token: token } = LeafNode.Repo.ExtensionToken.get_token_by_user(socket.assigns.current_user.id) || ""
+    socket = socket
+      |> assign(:nodes, get_nodes(socket))
+      |> assign(:extension_details, %{ id: id, token: token})
+    {:ok, socket}
   end
 
   # Render function
@@ -14,13 +18,34 @@ defmodule LeafNodeWeb.NodesLive do
     ~H"""
       <!-- Header -->
       <div>
-        <div class="flex justify-between items-center mt-10 mb-4">
-          <!-- Primary Button -->
-          <button
-            phx-click="node_create"
-            class="bg-blue-700 hover:bg-blue-600 py-2 px-4 text-sm rounded transition duration-200 ease-in-out">
-            Create
-          </button>
+        <div>
+          <div class="flex justify-between items-center mt-10 mb-4">
+            <!-- Primary Button -->
+            <div class="w-full ">
+              <button
+                phx-click="node_create"
+                class="bg-blue-700 hover:bg-blue-600 py-2 px-4 text-sm rounded transition duration-200 ease-in-out">
+                Create
+              </button>
+            </div>
+
+            <%!-- Here we show the relevant extension token --%>
+            <div class="w-auto flex justify-center gap-2">
+              <input
+                type="text"
+                id="disabled-input"
+                aria-label="disabled input"
+                class="box_input_inset_shadow disabled text-zinc-800 text-sm rounded-lg border-stone-900 block dark:text-gray-400"
+                value={@extension_details.token}
+                disabled
+              />
+              <button
+                phx-click="regenerate_extension_token"
+                class="bg-blue-700 hover:bg-blue-600 py-2 px-4 text-sm rounded transition duration-200 ease-in-out">
+                regenerate
+              </button>
+            </div>
+          </div>
         </div>
         <%= if is_nil(@nodes) or Kernel.length(@nodes) < 1 do %>
           <p class="text-gray-500"> There are no nodes, create a new one to get started</p>
@@ -67,6 +92,21 @@ defmodule LeafNodeWeb.NodesLive do
         {:ok, data} ->
           push_navigate(socket, to: "/dashboard/node/#{data.id}")
 
+        {:error, _err} ->
+          socket
+      end
+
+    {:noreply, socket}
+  end
+
+  def handle_event("regenerate_extension_token", _, socket) do
+    # user = socket.assigns.current_user
+    %{id: extension_id} = socket.assigns.extension_details
+    socket =
+      case LeafNode.Repo.ExtensionToken.regenerate_token(extension_id) do
+        {:ok, _} ->
+          updated_token = LeafNode.Repo.ExtensionToken.get_token(extension_id)
+          socket |> assign(:extension_details, updated_token)
         {:error, _err} ->
           socket
       end

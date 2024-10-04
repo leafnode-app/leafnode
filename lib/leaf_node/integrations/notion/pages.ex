5cef199a4752dc5b0a4c8cf852238033213dc21f
Torean Joel <toreanjoel@gmail.com>
2024-10-04 19:14:52 +0200

    feat: handle nested text values as part of the agent Notion page data

--- lib/leaf_node/integrations/notion/pages.ex
+++ lib/leaf_node/integrations/notion/pages.ex
@@ -2,11 +2,11 @@ defmodule LeafNode.Integrations.Notion.Pages do
   @moduledoc """
   The Notion integrations and helper functions in order to interact with it
   """
+  require Logger
   @notion_version "2021-05-13"
-  @block_limit 200
+  @block_limit 100
   @block_text_str_limit 500
 
-  # We dont need to process these keys for now
   @keys_to_remove [
     "archived",
     "created_by",
@@ -17,14 +17,12 @@ defmodule LeafNode.Integrations.Notion.Pages do
     "last_edited_by",
     "last_edited_time",
     "object",
-    "parent",
+    "parent"
   ]
 
-  # Form details
   def input_info() do
     [
       {"page_id", "Page Id", "text", "The notion page id you will grant access for"},
-      # {"content", "Content", "text", "Content for the Notion page. Use [[input.payload.any_key]] for dynamic data. E.g., [[input.payload.text_content]]."}
     ]
   end
 
@@ -39,8 +37,7 @@ defmodule LeafNode.Integrations.Notion.Pages do
     case HTTPoison.get(url, headers) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         # We extract text or plain text if possible
-        # We only return type and text value for the non nil types
-        data = Enum.reduce(drop_keys(Jason.decode!(body)["results"]), [], fn item, acc ->
+        data = Enum.reduce(drop_keys(Jason.decode!(body)["results"], token), [], fn item, acc ->
           if is_nil(item["text"]) do
             acc
           else
@@ -52,29 +49,56 @@ defmodule LeafNode.Integrations.Notion.Pages do
     end
   end
 
-  # Drop keyys we dont need and refute non relevant strings
-  defp drop_keys([]), do: %{}
-  defp drop_keys(data) do
+  # Drop keys we don't need and fetch nested text
+  defp drop_keys([], _token), do: %{}
+  defp drop_keys(data, token) do
     Enum.map(data, fn item ->
       type_data =
         Map.get(item, item["type"], %{})
 
       text_list = Map.get(type_data, "text")
-      extracted_text = extract_plain_text(text_list)
+      extracted_text = extract_plain_text(text_list, "plain_text")
+
+      text =
+        if item["has_children"] do
+          # Fetch children recursively and accumulate the text
+          {:ok, children} = fetch_children(item["id"], token)
+          extracted_text <> " " <> Enum.join(Enum.map(children, fn child -> child["text"] end), " ")
+        else
+          extracted_text
+        end
 
       Map.drop(item, @keys_to_remove)
-        |> Map.delete(item["type"])
-        |> Map.put("text", extracted_text)
+      |> Map.delete(item["type"])
+      |> Map.put("text", text)
     end)
   end
 
-  # We try get the text out the blocks
-  # TODO: we can make this better and not check index?
-  defp extract_plain_text(nil), do: nil
-  defp extract_plain_text([]), do: nil
-  defp extract_plain_text(data) do
+  # Extract the plain text from the block's rich_text objects
+  defp extract_plain_text(nil, _), do: nil
+  defp extract_plain_text([], _), do: nil
+  defp extract_plain_text(data, "plain_text") do
     str = Enum.at(data, 0)["plain_text"]
-    # we need to shorted the string of the text per block for token size
     str |> String.slice(0, @block_text_str_limit)
+  end
+  defp extract_plain_text(data, "text") do
+    Enum.at(data, 0)["text"]
+  end
+
+  # Fetch children blocks and return their processed text recursively
+  defp fetch_children(block_id, token) do
+    url = "https://api.notion.com/v1/blocks/#{block_id}/children"
+
+    headers = [
+      {"Authorization", "Bearer #{token}"},
+      {"Notion-Version", @notion_version}
+    ]
+
+    case HTTPoison.get(url, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        data = drop_keys(Jason.decode!(body)["results"], token)
+        {:ok, data}
+      _ -> {:error, "There was an error"}
+    end
   end
 end
